a164b09d69a7a91eab8e114b853ae6083e96352b
ShyamaleeAbeysinghe <shyamaleeabeysinghe.0@gmail.com>
2024-09-08 11:02:36 +0530

    add js

---
+++ js/weather.js
@@ -0,0 +1,311 @@
+let dateTime01 = document.getElementById("dateTime01")
+let temperature=document.getElementById("temperature")
+let humidity=document.getElementById("humidity")
+let windSpeed=document.getElementById("windSpeed")
+let weatherDescription=document.getElementById("weatherDescription")
+let feelTemperature=document.getElementById("feelTemperature")
+let alert01=document.getElementById("alert01")
+let tblAlert=document.getElementById("tblAlert")
+
+let todayClimate=document.getElementById("todayClimate")
+let imgToday01=document.getElementById("imgToday01")
+
+let imgForecast01=document.getElementById("imgForecast01")
+let imgForecast02=document.getElementById("imgForecast02")
+let imgForecast03=document.getElementById("imgForecast03")
+
+let forecastDay01=document.getElementById("forecastDay01")
+let forecastDay02=document.getElementById("forecastDay02")
+let forecastDay03=document.getElementById("forecastDay03")
+
+let forecastTemp01=document.getElementById("forecastTemp01")
+let forecastTemp02=document.getElementById("forecastTemp02")
+let forecastTemp03=document.getElementById("forecastTemp03")
+
+let tempMin01=document.getElementById("tempMin01")
+let tempMin02=document.getElementById("tempMin02")
+let tempMin03=document.getElementById("tempMin03")
+
+let tempMax01=document.getElementById("tempMax01")
+let tempMax02=document.getElementById("tempMax02")
+let tempMax03=document.getElementById("tempMax03")
+
+let imgHistort01=document.getElementById("imgHistort01")
+let imgHistort02=document.getElementById("imgHistort02")
+let imgHistort03=document.getElementById("imgHistort03")
+let imgHistort04=document.getElementById("imgHistort04")
+let imgHistort05=document.getElementById("imgHistort05")
+let imgHistort06=document.getElementById("imgHistort06")
+let imgHistort07=document.getElementById("imgHistort07")
+
+let historyDay01=document.getElementById("historyDay01")
+let historyDay02=document.getElementById("historyDay02")
+let historyDay03=document.getElementById("historyDay03")
+let historyDay04=document.getElementById("historyDay04")
+let historyDay05=document.getElementById("historyDay05")
+let historyDay06=document.getElementById("historyDay06")
+let historyDay07=document.getElementById("historyDay07")
+
+let historyTemp01=document.getElementById("historyTemp01")
+let historyTemp02=document.getElementById("historyTemp02")
+let historyTemp03=document.getElementById("historyTemp03")
+let historyTemp04=document.getElementById("historyTemp04")
+let historyTemp05=document.getElementById("historyTemp05")
+let historyTemp06=document.getElementById("historyTemp06")
+let historyTemp07=document.getElementById("historyTemp07")
+
+let historyMin01=document.getElementById("historyMin01")
+let historyMin02=document.getElementById("historyMin02")
+let historyMin03=document.getElementById("historyMin03")
+let historyMin04=document.getElementById("historyMin04")
+let historyMin05=document.getElementById("historyMin05")
+let historyMin06=document.getElementById("historyMin06")
+let historyMin07=document.getElementById("historyMin07")
+
+let historyMax01=document.getElementById("historyMax01")
+let historyMax02=document.getElementById("historyMax02")
+let historyMax03=document.getElementById("historyMax03")
+let historyMax04=document.getElementById("historyMax04")
+let historyMax05=document.getElementById("historyMax05")
+let historyMax06=document.getElementById("historyMax06")
+let historyMax07=document.getElementById("historyMax07")
+
+let hourlyTime01=document.getElementById("hourlyTime01")
+let hourlyTime02=document.getElementById("hourlyTime02")
+let hourlyTime03=document.getElementById("hourlyTime03")
+let hourlyTime04=document.getElementById("hourlyTime04")
+let hourlyTime05=document.getElementById("hourlyTime05")
+let hourlyTime06=document.getElementById("hourlyTime06")
+let hourlyTime07=document.getElementById("hourlyTime07")
+let hourlyTime08=document.getElementById("hourlyTime08")
+let hourlyTime09=document.getElementById("hourlyTime09")
+let hourlyTime10=document.getElementById("hourlyTime10")
+let hourlyTime11=document.getElementById("hourlyTime11")
+let hourlyTime12=document.getElementById("hourlyTime12")
+let hourlyTime13=document.getElementById("hourlyTime13")
+let hourlyTime14=document.getElementById("hourlyTime14")
+let hourlyTime15=document.getElementById("hourlyTime15")
+let hourlyTime16=document.getElementById("hourlyTime16")
+let hourlyTime17=document.getElementById("hourlyTime17")
+let hourlyTime18=document.getElementById("hourlyTime18")
+let hourlyTime19=document.getElementById("hourlyTime19")
+let hourlyTime20=document.getElementById("hourlyTime20")
+let hourlyTime21=document.getElementById("hourlyTime21")
+let hourlyTime22=document.getElementById("hourlyTime22")
+let hourlyTime23=document.getElementById("hourlyTime23")
+let hourlyTime24=document.getElementById("hourlyTime24")
+
+let hourlyTemp01=document.getElementById("hourlyTemp01")
+let hourlyTemp02=document.getElementById("hourlyTemp02")
+let hourlyTemp03=document.getElementById("hourlyTemp03")
+let hourlyTemp04=document.getElementById("hourlyTemp04")
+let hourlyTemp05=document.getElementById("hourlyTemp05")
+let hourlyTemp06=document.getElementById("hourlyTemp06")
+let hourlyTemp07=document.getElementById("hourlyTemp07")
+let hourlyTemp08=document.getElementById("hourlyTemp08")
+let hourlyTemp09=document.getElementById("hourlyTemp09")
+let hourlyTemp10=document.getElementById("hourlyTemp10")
+let hourlyTemp11=document.getElementById("hourlyTemp11")
+let hourlyTemp12=document.getElementById("hourlyTemp12")
+let hourlyTemp13=document.getElementById("hourlyTemp13")
+let hourlyTemp14=document.getElementById("hourlyTemp14")
+let hourlyTemp15=document.getElementById("hourlyTemp15")
+let hourlyTemp16=document.getElementById("hourlyTemp16")
+let hourlyTemp17=document.getElementById("hourlyTemp17")
+let hourlyTemp18=document.getElementById("hourlyTemp18")
+let hourlyTemp19=document.getElementById("hourlyTemp19")
+let hourlyTemp20=document.getElementById("hourlyTemp20")
+let hourlyTemp21=document.getElementById("hourlyTemp21")
+let hourlyTemp22=document.getElementById("hourlyTemp22")
+let hourlyTemp23=document.getElementById("hourlyTemp23")
+let hourlyTemp24=document.getElementById("hourlyTemp24")
+
+let hourlyImg01=document.getElementById("hourlyImg01")
+let hourlyImg02=document.getElementById("hourlyImg02")
+let hourlyImg03=document.getElementById("hourlyImg03")
+let hourlyImg04=document.getElementById("hourlyImg04")
+let hourlyImg05=document.getElementById("hourlyImg05")
+let hourlyImg06=document.getElementById("hourlyImg06")
+let hourlyImg07=document.getElementById("hourlyImg07")
+let hourlyImg08=document.getElementById("hourlyImg08")
+let hourlyImg09=document.getElementById("hourlyImg09")
+let hourlyImg10=document.getElementById("hourlyImg10")
+let hourlyImg11=document.getElementById("hourlyImg11")
+let hourlyImg12=document.getElementById("hourlyImg12")
+let hourlyImg13=document.getElementById("hourlyImg13")
+let hourlyImg14=document.getElementById("hourlyImg14")
+let hourlyImg15=document.getElementById("hourlyImg15")
+let hourlyImg16=document.getElementById("hourlyImg16")
+let hourlyImg17=document.getElementById("hourlyImg17")
+let hourlyImg18=document.getElementById("hourlyImg18")
+let hourlyImg19=document.getElementById("hourlyImg19")
+let hourlyImg20=document.getElementById("hourlyImg20")
+let hourlyImg21=document.getElementById("hourlyImg21")
+let hourlyImg22=document.getElementById("hourlyImg22")
+let hourlyImg23=document.getElementById("hourlyImg23")
+let hourlyImg24=document.getElementById("hourlyImg24")
+
+
+
+
+fetch("http://api.weatherapi.com/v1/forecast.json?key=10fd1d1e0ca24a2f99e101538242808&q=sri lanka&days=4&alerts=yes")
+.then(data =>data.json())
+    .then(data =>{
+        console.log(data);
+         dateTime01.innerHTML= data.location.localtime;
+         temperature.innerHTML= data.current.temp_c + "°C";
+         humidity.innerHTML= data.current.humidity+"%";
+         windSpeed.innerHTML= data.current.wind_kph+"kph"
+         weatherDescription.innerHTML= data.current.condition.text;
+         feelTemperature.innerHTML=data.current.feelslike_c+"°C";
+         if(data.alerts.alert.length==0){
+            tblAlert.style.visibility="collapse";
+            alert01.style.visibility="visible";
+         }else{
+            tblAlert.style.visibility="visible";
+            alert01.style.visibility="collapse";
+         }
+        imgForecast01.src=data.forecast.forecastday[1].day.condition.icon;
+        imgForecast02.src=data.forecast.forecastday[2].day.condition.icon;
+        imgForecast03.src=data.forecast.forecastday[3].day.condition.icon;
+
+        forecastDay01.innerHTML=data.forecast.forecastday[1].date;
+        forecastDay02.innerHTML=data.forecast.forecastday[2].date;
+        forecastDay03.innerHTML=data.forecast.forecastday[3].date;
+
+        forecastTemp01.innerHTML=data.forecast.forecastday[1].day.condition.text;
+        forecastTemp02.innerHTML=data.forecast.forecastday[2].day.condition.text;
+        forecastTemp03.innerHTML=data.forecast.forecastday[3].day.condition.text;
+
+        tempMin01.innerHTML=data.forecast.forecastday[1].day.mintemp_c+"°C";
+        tempMin02.innerHTML=data.forecast.forecastday[2].day.mintemp_c+"°C";
+        tempMin03.innerHTML=data.forecast.forecastday[3].day.mintemp_c+"°C";
+
+        tempMax01.innerHTML=data.forecast.forecastday[1].day.maxtemp_c+"°C";
+        tempMax02.innerHTML=data.forecast.forecastday[2].day.maxtemp_c+"°C";
+        tempMax03.innerHTML=data.forecast.forecastday[3].day.maxtemp_c+"°C";
+
+        todayClimate.innerHTML=data.current.condition.text;
+        imgToday01.src=data.current.condition.icon
+
+        hourlyTime01.innerHTML=data.forecast.forecastday[0].hour[0].time;
+        hourlyTime02.innerHTML=data.forecast.forecastday[0].hour[1].time;
+        hourlyTime03.innerHTML=data.forecast.forecastday[0].hour[2].time;
+        hourlyTime04.innerHTML=data.forecast.forecastday[0].hour[3].time;
+        hourlyTime05.innerHTML=data.forecast.forecastday[0].hour[4].time;
+        hourlyTime06.innerHTML=data.forecast.forecastday[0].hour[5].time;
+        hourlyTime07.innerHTML=data.forecast.forecastday[0].hour[6].time;
+        hourlyTime08.innerHTML=data.forecast.forecastday[0].hour[7].time;
+        hourlyTime09.innerHTML=data.forecast.forecastday[0].hour[8].time;
+        hourlyTime10.innerHTML=data.forecast.forecastday[0].hour[9].time;
+        hourlyTime11.innerHTML=data.forecast.forecastday[0].hour[10].time;
+        hourlyTime12.innerHTML=data.forecast.forecastday[0].hour[11].time;
+        hourlyTime13.innerHTML=data.forecast.forecastday[0].hour[12].time;
+        hourlyTime14.innerHTML=data.forecast.forecastday[0].hour[13].time;
+        hourlyTime15.innerHTML=data.forecast.forecastday[0].hour[14].time;
+        hourlyTime16.innerHTML=data.forecast.forecastday[0].hour[15].time;
+        hourlyTime17.innerHTML=data.forecast.forecastday[0].hour[16].time;
+        hourlyTime18.innerHTML=data.forecast.forecastday[0].hour[17].time;
+        hourlyTime19.innerHTML=data.forecast.forecastday[0].hour[18].time;
+        hourlyTime20.innerHTML=data.forecast.forecastday[0].hour[19].time;
+        hourlyTime21.innerHTML=data.forecast.forecastday[0].hour[20].time;
+        hourlyTime22.innerHTML=data.forecast.forecastday[0].hour[21].time;
+        hourlyTime23.innerHTML=data.forecast.forecastday[0].hour[22].time;
+        hourlyTime24.innerHTML=data.forecast.forecastday[0].hour[23].time;
+
+        hourlyTemp01.innerHTML=data.forecast.forecastday[0].hour[0].temp_c+"°C";
+        hourlyTemp02.innerHTML=data.forecast.forecastday[0].hour[1].temp_c+"°C";
+        hourlyTemp03.innerHTML=data.forecast.forecastday[0].hour[2].temp_c+"°C";
+        hourlyTemp04.innerHTML=data.forecast.forecastday[0].hour[3].temp_c+"°C";
+        hourlyTemp05.innerHTML=data.forecast.forecastday[0].hour[4].temp_c+"°C";
+        hourlyTemp06.innerHTML=data.forecast.forecastday[0].hour[5].temp_c+"°C";
+        hourlyTemp07.innerHTML=data.forecast.forecastday[0].hour[6].temp_c+"°C";
+        hourlyTemp08.innerHTML=data.forecast.forecastday[0].hour[7].temp_c+"°C";
+        hourlyTemp09.innerHTML=data.forecast.forecastday[0].hour[8].temp_c+"°C";
+        hourlyTemp10.innerHTML=data.forecast.forecastday[0].hour[9].temp_c+"°C";
+        hourlyTemp11.innerHTML=data.forecast.forecastday[0].hour[10].temp_c+"°C";
+        hourlyTemp12.innerHTML=data.forecast.forecastday[0].hour[11].temp_c+"°C";
+        hourlyTemp13.innerHTML=data.forecast.forecastday[0].hour[12].temp_c+"°C";
+        hourlyTemp14.innerHTML=data.forecast.forecastday[0].hour[13].temp_c+"°C";
+        hourlyTemp15.innerHTML=data.forecast.forecastday[0].hour[14].temp_c+"°C";
+        hourlyTemp16.innerHTML=data.forecast.forecastday[0].hour[15].temp_c+"°C";
+        hourlyTemp17.innerHTML=data.forecast.forecastday[0].hour[16].temp_c+"°C";
+        hourlyTemp18.innerHTML=data.forecast.forecastday[0].hour[17].temp_c+"°C";
+        hourlyTemp19.innerHTML=data.forecast.forecastday[0].hour[18].temp_c+"°C";
+        hourlyTemp20.innerHTML=data.forecast.forecastday[0].hour[19].temp_c+"°C";
+        hourlyTemp21.innerHTML=data.forecast.forecastday[0].hour[20].temp_c+"°C";
+        hourlyTemp22.innerHTML=data.forecast.forecastday[0].hour[21].temp_c+"°C";
+        hourlyTemp23.innerHTML=data.forecast.forecastday[0].hour[22].temp_c+"°C";
+        hourlyTemp24.innerHTML=data.forecast.forecastday[0].hour[23].temp_c+"°C";
+
+        hourlyImg01.src=data.forecast.forecastday[0].hour[0].condition.icon;
+        hourlyImg02.src=data.forecast.forecastday[0].hour[1].condition.icon;
+        hourlyImg03.src=data.forecast.forecastday[0].hour[2].condition.icon;
+        hourlyImg04.src=data.forecast.forecastday[0].hour[3].condition.icon;
+        hourlyImg05.src=data.forecast.forecastday[0].hour[4].condition.icon;
+        hourlyImg06.src=data.forecast.forecastday[0].hour[5].condition.icon;
+        hourlyImg07.src=data.forecast.forecastday[0].hour[6].condition.icon;
+        hourlyImg08.src=data.forecast.forecastday[0].hour[7].condition.icon;
+        hourlyImg09.src=data.forecast.forecastday[0].hour[8].condition.icon;
+        hourlyImg10.src=data.forecast.forecastday[0].hour[9].condition.icon;
+        hourlyImg11.src=data.forecast.forecastday[0].hour[10].condition.icon;
+        hourlyImg12.src=data.forecast.forecastday[0].hour[11].condition.icon;
+        hourlyImg13.src=data.forecast.forecastday[0].hour[12].condition.icon;
+        hourlyImg14.src=data.forecast.forecastday[0].hour[13].condition.icon;
+        hourlyImg15.src=data.forecast.forecastday[0].hour[14].condition.icon;
+        hourlyImg16.src=data.forecast.forecastday[0].hour[15].condition.icon;
+        hourlyImg17.src=data.forecast.forecastday[0].hour[16].condition.icon;
+        hourlyImg18.src=data.forecast.forecastday[0].hour[17].condition.icon;
+        hourlyImg19.src=data.forecast.forecastday[0].hour[18].condition.icon;
+        hourlyImg20.src=data.forecast.forecastday[0].hour[19].condition.icon;
+        hourlyImg21.src=data.forecast.forecastday[0].hour[20].condition.icon;
+        hourlyImg22.src=data.forecast.forecastday[0].hour[21].condition.icon;
+        hourlyImg23.src=data.forecast.forecastday[0].hour[22].condition.icon;
+        hourlyImg24.src=data.forecast.forecastday[0].hour[23].condition.icon;
+
+      })
+
+      fetch("http://api.weatherapi.com/v1/history.json?key=10fd1d1e0ca24a2f99e101538242808&q=sri lanka&dt=2024-08-27&end_dt=2024-09-02")
+.then(data =>data.json())
+    .then(data =>{
+      console.log(data);
+      imgHistort01.src=data.forecast.forecastday[0].day.condition.icon;
+      imgHistort02.src=data.forecast.forecastday[1].day.condition.icon;
+      imgHistort03.src=data.forecast.forecastday[2].day.condition.icon;
+      imgHistort04.src=data.forecast.forecastday[3].day.condition.icon;
+      imgHistort05.src=data.forecast.forecastday[4].day.condition.icon;
+      imgHistort06.src=data.forecast.forecastday[5].day.condition.icon;
+      imgHistort07.src=data.forecast.forecastday[6].day.condition.icon;
+
+      historyDay01.innerHTML=data.forecast.forecastday[0].date;
+      historyDay02.innerHTML=data.forecast.forecastday[1].date;
+      historyDay03.innerHTML=data.forecast.forecastday[2].date;
+      historyDay04.innerHTML=data.forecast.forecastday[3].date;
+      historyDay05.innerHTML=data.forecast.forecastday[4].date;
+      historyDay06.innerHTML=data.forecast.forecastday[5].date;
+      historyDay07.innerHTML=data.forecast.forecastday[6].date;
+
+      historyTemp01.innerHTML=data.forecast.forecastday[0].day.condition.text;
+      historyTemp02.innerHTML=data.forecast.forecastday[1].day.condition.text;
+      historyTemp03.innerHTML=data.forecast.forecastday[2].day.condition.text;
+      historyTemp04.innerHTML=data.forecast.forecastday[3].day.condition.text;
+      historyTemp05.innerHTML=data.forecast.forecastday[4].day.condition.text;
+      historyTemp06.innerHTML=data.forecast.forecastday[5].day.condition.text;
+      historyTemp07.innerHTML=data.forecast.forecastday[6].day.condition.text;
+
+      historyMin01.innerHTML=data.forecast.forecastday[0].day.mintemp_c+"°C";
+      historyMin02.innerHTML=data.forecast.forecastday[1].day.mintemp_c+"°C";
+      historyMin03.innerHTML=data.forecast.forecastday[2].day.mintemp_c+"°C";
+      historyMin04.innerHTML=data.forecast.forecastday[3].day.mintemp_c+"°C";
+      historyMin05.innerHTML=data.forecast.forecastday[4].day.mintemp_c+"°C";
+      historyMin06.innerHTML=data.forecast.forecastday[5].day.mintemp_c+"°C";
+      historyMin07.innerHTML=data.forecast.forecastday[6].day.mintemp_c+"°C";
+
+      historyMax01.innerHTML=data.forecast.forecastday[0].day.maxtemp_c+"°C";
+      historyMax02.innerHTML=data.forecast.forecastday[1].day.maxtemp_c+"°C";
+      historyMax03.innerHTML=data.forecast.forecastday[2].day.maxtemp_c+"°C";
+      historyMax04.innerHTML=data.forecast.forecastday[3].day.maxtemp_c+"°C";
+      historyMax05.innerHTML=data.forecast.forecastday[4].day.maxtemp_c+"°C";
+      historyMax06.innerHTML=data.forecast.forecastday[5].day.maxtemp_c+"°C";
+      historyMax07.innerHTML=data.forecast.forecastday[6].day.maxtemp_c+"°C";
+    })
