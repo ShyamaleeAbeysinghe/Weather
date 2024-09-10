a18f3fa1c86eeeff44a294824690cd685fde716b
ShyamaleeAbeysinghe <shyamaleeabeysinghe.0@gmail.com>
2024-09-10 22:28:48 +0530

    live map integration and location search

--- js/weather.js
+++ js/weather.js
@@ -9,32 +9,40 @@ let tblAlert = document.getElementById("tblAlert")
 
 let todayClimate = document.getElementById("todayClimate")
 let imgToday01 = document.getElementById("imgToday01")
+let region = document.getElementById("region")
 
 let forecastData;
 let historyData;
 
-function loadPage() {
-   fetch("http://api.weatherapi.com/v1/forecast.json?key=10fd1d1e0ca24a2f99e101538242808&q=sri lanka&days=4&alerts=yes")
+
+
+
+function loadPage(region) {
+   fetch("http://api.weatherapi.com/v1/forecast.json?key=10fd1d1e0ca24a2f99e101538242808&q=" + region + "&days=4&alerts=yes")
       .then(data => data.json())
       .then(data => {
          forecastData = data;
       }).then(function (json) {
-         loadForecastData("Celcius");
+         loadForecastData("Celcious");
       })
 
-   fetch("http://api.weatherapi.com/v1/history.json?key=10fd1d1e0ca24a2f99e101538242808&q=sri lanka&dt=2024-08-27&end_dt=2024-09-02")
+   var d = new Date();
+   var yesterday = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + (d.getDate() );
+   var lastweek = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + (d.getDate() - 7);
+
+   fetch("http://api.weatherapi.com/v1/history.json?key=10fd1d1e0ca24a2f99e101538242808&q=" + region + "&dt="+lastweek+"&end_dt="+yesterday)
       .then(data => data.json())
       .then(data => {
          historyData = data;
       }).then(function (json) {
-         loadHistoryData("Celcius")
+         loadHistoryData("Celcious")
       })
 
 
 }
 
 function loadForecastData(temperatureInput) {
-   dateTime01.innerHTML = forecastData.location.localtime;
+
    humidity.innerHTML = forecastData.current.humidity + "%";
    windSpeed.innerHTML = forecastData.current.wind_kph + "kph"
    weatherDescription.innerHTML = forecastData.current.condition.text;
@@ -80,7 +88,7 @@ function loadForecastData(temperatureInput) {
 
 
 
-   if (temperatureInput == "Celcius") {
+   if (temperatureInput == "Celcious") {
       temperature.innerHTML = forecastData.current.temp_c + "°C";
       feelTemperature.innerHTML = forecastData.current.feelslike_c + "°C";
 
@@ -148,11 +156,11 @@ function loadHistoryData(temperatureInput) {
       let historyTemp = document.getElementById("historyTemp0" + tempIndex)
       historyTemp.innerHTML = historyData.forecast.forecastday[index].day.condition.text;
 
-     
+
 
    }
 
-   if (temperatureInput == "Celcius") {
+   if (temperatureInput == "Celcious") {
       for (var index = 0; index < 7; index++) {
          var tempIndex = index;
          tempIndex++;
@@ -185,9 +193,16 @@ function loadHistoryData(temperatureInput) {
    }
 }
 
+function loadTime() {
+   const d = new Date();
+   dateTime01.innerHTML = d.toLocaleString();
+}
 
+setInterval(() => {
+   loadTime();
+}, 1000);
 
-loadPage();
+loadPage("sri lanka");
 
 
 
@@ -253,4 +268,133 @@ document.getElementById('togglebtn').addEventListener('click', () => {
 
    }
 
-})
+});
+
+document.getElementById('tempDropdown').addEventListener('change', () => {
+   var tempSelect = document.getElementById('tempDropdown').value;
+   loadForecastData(tempSelect);
+   loadHistoryData(tempSelect)
+});
+
+
+
+document.getElementById('searchRegion').addEventListener('input', () => {
+   var searchRegion = document.getElementById('searchRegion').value;
+   if (searchRegion != "") {
+
+      fetch("http://api.weatherapi.com/v1/search.json?key=10fd1d1e0ca24a2f99e101538242808&q=" + searchRegion)
+         .then(data => data.json())
+         .then(data => {
+            var datalist = "";
+            data.forEach(element => {
+               datalist += `<option value="${element.name}" />`
+            });
+
+            region.innerHTML = datalist;
+         })
+      var opt = document.getElementById('region').children;
+      for (var i = 0; i < opt.length; i++) {
+         if (opt[i].value == searchRegion) {
+            loadPage(searchRegion)
+            break;
+
+         }
+      }
+   }
+
+});
+
+const options = {method: 'GET', headers: {accept: 'text/plain'}};
+
+maptilersdk.config.apiKey = '8HLSaAqVVlNgnJYfj6iW';
+      const map = (window.map = new maptilersdk.Map({
+        container: 'map', // container's id or the HTML element to render the map
+        style: maptilersdk.MapStyle.BACKDROP,
+        zoom: 2,
+        center: [0, 40]
+      }));
+
+      const timeInfoContainer = document.getElementById("time-info");
+      const timeTextDiv = document.getElementById("time-text");
+      const timeSlider = document.getElementById("time-slider");
+      const playPauseButton = document.getElementById("play-pause-bt");
+      const pointerDataDiv = document.getElementById("pointer-data");
+      let pointerLngLat = null;
+
+      const weatherLayer = new maptilerweather.WindLayer();
+
+      map.on('load', function () {
+        map.setPaintProperty("Water", 'fill-color', "rgba(0, 0, 0, 0.4)");
+        map.addLayer(weatherLayer, 'Water');
+      });
+
+      timeSlider.addEventListener("input", (evt) => {
+        weatherLayer.setAnimationTime(parseInt(timeSlider.value / 1000))
+      });
+
+      // Event called when all the datasource for the next days are added and ready.
+      // From now on, the layer nows the start and end dates.
+      weatherLayer.on("sourceReady", event => {
+        const startDate = weatherLayer.getAnimationStartDate();
+        const endDate = weatherLayer.getAnimationEndDate();
+        const currentDate = weatherLayer.getAnimationTimeDate();
+        refreshTime()
+
+        timeSlider.min = +startDate;
+        timeSlider.max = +endDate;
+        timeSlider.value = +currentDate;
+      });
+
+      // Called when the animation is progressing
+      weatherLayer.on("tick", event => {
+        refreshTime();
+        updatePointerValue(pointerLngLat);
+      });
+
+      // Called when the time is manually set
+      weatherLayer.on("animationTimeSet", event => {
+        refreshTime()
+      });
+
+      // When clicking on the play/pause
+      let isPlaying = false;
+      playPauseButton.addEventListener("click", () => {
+        if (isPlaying) {
+          weatherLayer.animateByFactor(0);
+          playPauseButton.innerText = "Play 3600x";
+        } else {
+          weatherLayer.animateByFactor(3600);
+          playPauseButton.innerText = "Pause";
+        }
+
+        isPlaying = !isPlaying;
+      });
+
+      // Update the date time display
+      function refreshTime() {
+        const d = weatherLayer.getAnimationTimeDate();
+        timeTextDiv.innerText = d.toString();
+        timeSlider.value = +d;
+      }
+
+      map.on('mouseout', function(evt) {
+        if (!evt.originalEvent.relatedTarget) {
+          pointerDataDiv.innerText = "";
+          pointerLngLat = null;
+        }
+      });
+
+      function updatePointerValue(lngLat) {
+        if (!lngLat) return;
+        pointerLngLat = lngLat;
+        const value = weatherLayer.pickAt(lngLat.lng, lngLat.lat);
+        if (!value) {
+          pointerDataDiv.innerText = "";
+          return;
+        }
+        pointerDataDiv.innerText = `${value.speedMetersPerSecond.toFixed(1)} m/s`
+      }
+
+      map.on('mousemove', (e) => {
+        updatePointerValue(e.lngLat);
+      });
